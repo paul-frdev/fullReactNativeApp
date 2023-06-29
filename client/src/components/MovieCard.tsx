be85bcbdc0437f6ae2a306835f8d7646299ff9bb
Админ <admin@MacBook-Pro-Admin.local>
2023-06-29 10:08:12 +0300

    add: carousel

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -1,14 +1,24 @@
 import React, { FC } from 'react'
-import { TouchableOpacity } from 'react-native'
+import { Dimensions, Image, Text, TouchableOpacity } from 'react-native'
+import { View } from 'react-native-animatable';
 
 
 interface MovieCardProps {
   movie: number;
 }
 export const MovieCard: FC<MovieCardProps> = ({ movie }) => {
+  const { width, height } = Dimensions.get('window')
   return (
-    <TouchableOpacity>
-
-    </TouchableOpacity>
+    <View className='flex justify-center items-center w-full'>
+      <TouchableOpacity>
+        <Image className='rouded-3xl bg-cover w-full' source={require('../assets/moviePoster2.png')} style={{
+          width: width * 0.8,
+          height: height * 0.52
+        }} />
+        <Text className='text-white text-3xl'>
+          {movie}
+        </Text>
+      </TouchableOpacity>
+    </View>
   )
 }
--- client/src/components/TrendingMoviesCarousel.tsx
+++ client/src/components/TrendingMoviesCarousel.tsx
@@ -1,23 +1,34 @@
 import React, { FC } from 'react'
-import { Text, View } from 'react-native'
-import Carousel from "react-native-snap-carousel"
+import { Dimensions, Text, View } from 'react-native'
+import Carousel from 'react-native-reanimated-carousel'
 import { MovieCard } from './MovieCard'
 
 interface TrendingMoviesCarouselProps {
   data: number[]
 }
 export const TrendingMoviesCarousel: FC<TrendingMoviesCarouselProps> = ({ data }) => {
+  const width = Dimensions.get('window').width;
+
   return (
-    <View className='mb-8'>
+    <View className='mb-8 w-full items-center'>
       <Text className='mt-10 text-white text-xl font-bold mb-5'>Trending</Text>
       <Carousel
         data={data}
+        style={{
+          width: width,
+        }}
         renderItem={({ item }) => <MovieCard movie={item} />}
-        firstItem={1}
-        inactiveSlideOpacity={0.6}
-        sliderWidth={600}
-        itemWidth={400}
-        slideStyle={{ display: 'flex', alignItems: 'center' }}
+        autoPlayInterval={3000}
+        mode="parallax"
+        modeConfig={{
+          parallaxScrollingScale: 0.9,
+          parallaxScrollingOffset: 110,
+        }}
+        loop
+        width={width}
+        height={width}
+        autoPlay={true}
+        scrollAnimationDuration={1000}
       />
     </View>
   )
